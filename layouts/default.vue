312558af7bedf298c526558d0353a5a0d7ce250c
Voice Button United Studio <62581450+VoiceButtonUnitedProject@users.noreply.github.com>
2020-12-03 13:00:18 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -149,7 +149,7 @@
           </div>
           <div>
             <span>Powered by</span>
-            <a href="https://www.vercel.com/?utm_source=oruyanke&utm_campaign=oss" target="_blank" rel="noreferrer">
+            <a href="https://www.vercel.com/?utm_source=vbuposc&utm_campaign=oss" target="_blank" rel="noreferrer">
               <img :src="vercel_logo" alt="vercel" height="16px" />
             </a>
           </div>
